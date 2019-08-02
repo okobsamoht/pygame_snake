4ed62253e0fbba1e7116b64091c4ef1c0c7ec40b
Thomas BOKO <bokothomas@yahoo.fr>
2019-08-02 22:39:37 +0100

    first

---
+++ src/Config.py
@@ -0,0 +1,25 @@
+Config = {
+    'game': {
+        'caption': 'Snake',
+        'height': 800,
+        'width': 800,
+        'fps': 45,
+        'bumper_size': 30
+    },
+    'snake': {
+        'height': 20,
+        'width': 20,
+        'speed': 2
+    },
+    'apple': {
+        'width': 20,
+        'height': 20,
+        'image': './assets/apple.png'
+    },
+    'colors': {
+        'white': (255, 255, 255),
+        'black': (0, 0, 0),
+        'green': (0, 100, 0),
+        'red': (100, 0, 0)
+    }
+}
---
+++ src/Game.py
@@ -0,0 +1,114 @@
+import pygame 
+
+from src.Config import Config
+from src.Snake import Snake
+from src.Apple import Apple
+
+class Game:
+    def __init__(self, display):
+        self.display = display
+        self.score = 0
+
+    def loop(self):
+        clock = pygame.time.Clock()
+        snake = Snake(self.display)
+        apple = Apple(self.display)
+
+        x_change = 0
+        y_change = 0
+        
+        self.score = 0
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        x_change = -Config['snake']['speed']
+                        y_change = 0
+                    elif event.key == pygame.K_RIGHT:
+                        x_change = Config['snake']['speed']
+                        y_change = 0
+                    elif event.key == pygame.K_UP:
+                        x_change = 0
+                        y_change = -Config['snake']['speed']
+                    elif event.key == pygame.K_DOWN:
+                        x_change = 0
+                        y_change = Config['snake']['speed']
+        
+            # Fill background and draw game area
+            self.display.fill(Config['colors']['green'])
+            
+            pygame.draw.rect(
+                self.display, 
+                Config['colors']['black'],
+                [
+                    Config['game']['bumper_size'],
+                    Config['game']['bumper_size'],
+                    Config['game']['height'] - Config['game']['bumper_size'] * 2,
+                    Config['game']['width'] - Config['game']['bumper_size'] * 2
+                ]
+            )
+            
+            # Draw an apple
+            apple_rect = apple.draw()
+
+            # Move and Re-Draw Snake
+            snake.move(x_change, y_change)
+            snake_rect = snake.draw()
+            snake.draw_body()
+
+            # Detect collison with wall
+            bumper_x = Config['game']['width'] - Config['game']['bumper_size']
+            bumper_y = Config['game']['height'] - Config['game']['bumper_size']
+
+            if (
+                snake.x_pos < Config['game']['bumper_size'] or
+                snake.y_pos < Config['game']['bumper_size'] or
+                snake.x_pos + Config['snake']['width'] > bumper_x or
+                snake.y_pos + Config['snake']['height'] > bumper_y
+            ):
+                self.loop()
+
+            # Detect collision with apple
+            if apple_rect.colliderect(snake_rect):
+                apple.randomize()
+                self.score += 1
+                snake.eat()
+
+            # Collide with Self
+            if len(snake.body) >= 1:
+                for cell in snake.body:
+                    if snake.x_pos == cell[0] and snake.y_pos == cell[1]:
+                        self.loop()
+
+            # Initialize font and draw title and score text
+            pygame.font.init()
+            font = pygame.font.Font('./assets/Now-Regular.otf', 28)
+            
+            score_text = 'Score: {}'.format(self.score)
+            score = font.render(score_text, True, Config['colors']['white'])
+            title = font.render(Config['game']['caption'], True, Config['colors']['white'])
+
+
+            title_rect = title.get_rect(
+                center=(
+                    Config['game']['width'] / 2, 
+                    Config['game']['bumper_size'] / 2
+                )
+            )
+
+            score_rect = score.get_rect(
+                center=(
+                    500/2, 
+                    Config['game']['height'] - Config['game']['bumper_size'] / 2
+                )
+            )
+
+            self.display.blit(score, score_rect)
+            self.display.blit(title, title_rect)
+
+            pygame.display.update()
+            clock.tick(Config['game']['fps'])
